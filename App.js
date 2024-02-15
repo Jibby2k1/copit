3ea2d94a13164ec4f143c314584f30e0aa197e4c
Raul <rvalle1@ufl.edu>
2024-02-15 17:09:28 -0500

    added crappy expandable bar for tabs

--- App.js
+++ App.js
@@ -1,24 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, SafeAreaView, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, Image, SafeAreaView, View, TouchableOpacity, Animated } from 'react-native';
 import React, { useState } from 'react'; 
 
 const App = () => {
 
   const [currentImage, setCurrentImage] = useState('image1');
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [widthAnim] = useState(new Animated.Value(0));
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+    Animated.timing(widthAnim, {
+      toValue: isExpanded ? 0 : 200,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
 
   const handlePress = () => {
     setCurrentImage(prevImage => prevImage === 'image1' ? 'image2' : 'image1');
   };
 
   return (
-    <TouchableOpacity onPress={handlePress}>
-      <Image
-        source={currentImage === 'image1' ? require('./assets/redbobsta.jpg') : require('./assets/saitama.jpg')}
-        style={{ 
-          height: 300,
-          width: 300}}
-      />
-    </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity 
+        onPress={handlePress}>
+        <Image
+          source={currentImage === 'image1' ? require('./assets/redbobsta.jpg') : require('./assets/saitama.jpg')}
+          style={styles.media}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+       style = {styles.expandButton}
+       onPress={toggleExpand}>
+      <Text> {isExpanded ? 'Collapse' : 'Expand'}</Text>
+      </TouchableOpacity>
+      <Animated.View style={{...styles.collapsibleContent, width: widthAnim }}>
+        <Text> LOLOLOL</Text>
+      </Animated.View>
+    </SafeAreaView>
   );
 }
 
@@ -26,9 +46,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
   },
+  media: {
+    height: 400,
+    width: 400,
+  },
+  expandButton: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    marginBottom: 10,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  collapsibleContent: {
+    backgroundColor: '#c0c0c0',
+    padding: 20,
+    marginBottom: 10,
+    marginRight: 10,
+    borderRadius: 5,
+  }
 });
 
 export default App;
