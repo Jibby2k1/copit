37f9ed7c64c521fc91b3a05479cbbdbfe7172614
Frank Sabates <fsabates@ufl.edu>
2024-04-14 21:18:03 -0400

    Improved Swiping feature and adding functionality to wishlist

--- Wishlist.tsx
+++ Wishlist.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet, View, FlatList, TouchableOpacity, Image } from 'react-native';
+import data from './data';
 
-const LikedComponent = () => {
+const LikedComponent = ({ wishlist, setWishlist }) => {
   
-  const wishlist = [
-    { id: '1', name: 'Dress', price: '$49.99', picture: require('./assets/dress.jpg') },
-    { id: '3', name: 'Jacket', price: '$120.00', picture: require('./assets/Jacket.jpg') },
+  const deleteItem = (id) => {
+    setWishlist(wishlist.filter(item => item.id !== id));
+  };
 
-  ];
-  
   return (
     <View style={styles.background}>
       <Text style={styles.title}>Wishlist</Text>
@@ -16,10 +15,10 @@ const LikedComponent = () => {
         data={wishlist}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item}>
-            <Image source={item.picture} style={styles.image} resizeMode="contain"/>
-            <Text style={styles.text}>{item.name}</Text>
-            <Text style={styles.text}>{item.price}</Text>
+          <TouchableOpacity style={styles.item} onPress={() => deleteItem(item.id)}>
+            <Image source={{uri: item.image}} style={styles.image} resizeMode="contain"/>
+            <Text style={styles.text}>{item.productName}</Text>
+            <Text style={styles.price} numberOfLines={1} ellipsizeMode='tail'>{item.price}</Text>
           </TouchableOpacity>
         )}
       />
@@ -38,17 +37,28 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    textAlign: 'center',
   },
   item: {
     backgroundColor: '#333',
     padding: 10,
     marginBottom: 10,
     flexDirection: 'row',
-    justifyContent: 'space-between',
+  },
+  price: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 5,
+    textAlign: 'center',
   },
   text: {
     color: 'white',
-    fontSize: 20, 
+    flexShrink: 1,
+    fontSize: 18, 
+    fontWeight: 'bold',
+    marginLeft: 5, // add some space to the left of the text
+    textAlign: 'center', // center the text
   },
   image: {
     width: 100,
---
+++ data.js
@@ -0,0 +1,125 @@
+export default[
+    {
+        id: '1',
+        price: '$235.00',
+        productName: 'Custom Nike Air Force 1',
+        image: 'https://i.ebayimg.com/images/g/JesAAOSwtqxjNFaR/s-l1600.jpg'
+    },
+    {
+        id: '2',
+        price: '$84.99',
+        productName: 'Levi\'s 501 Original Fit Jeans',
+        image: 'https://lsco.scene7.com/is/image/lsco/125010400-alt2-pdp-lse?fmt=jpeg&qlt=70&resMode=bisharp&fit=crop,1&op_usm=1.25,0.6,8&wid=2000&hei=1800'
+    },
+    {
+        id: '3',
+        price: '$26.00',
+        productName: 'Adidas Originals Cap',
+        image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ef503debdc4d4f4ca8dfaa0f0044e95c_9366/Relaxed_Strap-Back_Hat_Grey_CL5247_01_standard.jpg'
+    },
+
+    // 4 Not working
+    {
+        id: '4',
+        price: '$930.00',
+        productName: 'Gucci GG Marmont mini bag',
+        image: 'https://i.ebayimg.com/images/g/PI4AAOSwhxRjyI48/s-l1200.webp'
+    },
+    {
+        id: '5',
+        price: '$120.99',
+        productName: 'Puma Ignite Sneakers',
+        image: 'https://www.tradeinn.com/f/13635/136355710/puma-ignite-limitless-shoes.jpg'
+    },
+    {
+        id: '6',
+        price: '$75.99',
+        productName: 'Calvin Klein Slim Fit Jeans',
+        image: 'https://cdna.lystit.com/photos/calvinklein/9680cfe6/calvin-klein-GREY-BRICK-Slim-Straight-Faded-Grey-Jeans.jpeg'
+    },
+    {
+        id: '7',
+        price: '$16.99',
+        productName: 'Under Armour Sports Cap',
+        image: 'https://m.media-amazon.com/images/I/91m3KE5QGkL._AC_UY1000_.jpg'
+    },
+    {
+        id: '8',
+        price: '$21.99',
+        productName: 'Tommy Hilfiger Leather Belt',
+        image: 'https://cdn-images.farfetch-contents.com/20/26/81/52/20268152_50469939_600.jpg'
+    },
+    {
+        id: '9',
+        price: '$71.99',
+        productName: 'Ralph Lauren Polo Shirt',
+        image: 'https://i.ebayimg.com/images/g/FJcAAOSwH8hkeVpq/s-l1600.jpg'
+    },
+    {
+        id: '10',
+        price: '$337.99',
+        productName: 'Burberry Check Scarf',
+        image: 'https://i.ebayimg.com/00/s/MTQ0MFgxMDU1/z/3q0AAOSwcpFitLhW/$_12.JPG?set_id=880000500F'
+    },
+    // 11 slowly loading
+    {
+        id: '11',
+        price: '$795.00',
+        productName: 'Prada Saffiano Wallet',
+        image: 'https://cdn-images.farfetch-contents.com/13/32/25/36/13322536_15056235_600.jpg'
+    },
+    {
+        id: '12',
+        price: '$69.99',
+        productName: 'Lacoste Classic Polo',
+        image: 'https://data.mascheronistore.com/imgprodotto/polo-piquet-classic-fit-man-green_121112_zoom.jpg'
+    },
+    {
+        id: '13',
+        price: '$54.99',
+        productName: 'Vans Old Skool Sneakers',
+        image: 'https://m.media-amazon.com/images/I/81R4EBqHIUL._AC_UY900_.jpg'
+    },
+    {
+        id: '14',
+        price: '$65.00',
+        productName: 'Converse Chuck Taylor All Star',
+        image: 'https://m.media-amazon.com/images/I/71aG5xlbY1L._AC_UY900_.jpg'
+    },
+    {
+        id: '15',
+        price: '$190.00',
+        productName: 'Adidas Ultraboost Running Shoes',
+        image: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/3c6babef3d3240d3969b701f92ae1ded_9366/Ultraboost_Light_Running_Shoes_Black_HP9477.jpg'
+    },
+    {
+        id: '16',
+        price: '$45.99',
+        productName: 'Zara Man Slim Fit Chinos',
+        image: 'https://static.zara.net/assets/public/0d1b/1d44/a6994899b73e/abdecd2da478/1679483127828/1679483127828.jpg?ts=1704679999924&w=1920'
+    },
+    {
+        id: '17',
+        price: '$5.99',
+        productName: 'H&M Crew Neck T-Shirt',
+        image: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F26%2F4c%2F264c077f065c7e2deb1b99d6b14881f39ae55eba.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]'
+    },
+    {
+        id: '18',
+        price: '$49.99',
+        productName: 'Uniqlo Cashmere Sweater',
+        image: 'https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/438791/item/goods_02_438791.jpg'
+    },
+    {
+        id: '19',
+        price: '$170.99',
+        productName: 'Ray-Ban Aviator Sunglasses',
+        image: 'https://assets.lenscrafters.com/is/image/LensCrafters/805289602057__STD__shad__cfr.png?impolicy=LC_grey'
+    },
+    {
+        id: '20',
+        price: '$120.99',
+        productName: 'Casio G-Shock Watch',
+        image: 'https://www.military.com/sites/default/files/styles/full/public/2023-11/1time-casio-g-shock-watches-1800.jpg'
+    }
+];
